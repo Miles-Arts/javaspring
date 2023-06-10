b3e9f82d1d27de0208f520601c1bb34f183f007a
Milton Figueredo Miles <gratno@gmail.com>
2023-06-10 15:13:20 -0500

    Create fileExtensión and randomUUID

--- src/main/java/com/subida/archivos/services/impl/UploadFilesServiceImpl.java
+++ src/main/java/com/subida/archivos/services/impl/UploadFilesServiceImpl.java
@@ -3,11 +3,49 @@ package com.subida.archivos.services.impl;
 import com.subida.archivos.services.IUploadFilesService;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
 public class UploadFilesServiceImpl implements IUploadFilesService {
 
 
     @Override
     public String handleFileUpload(MultipartFile file) throws Exception {
-        return null;
+
+        try {
+            String fileName = UUID.randomUUID().toString();
+            byte[] bytes = file.getBytes();
+            String fileOriginalName = file.getOriginalFilename();
+
+            long fileSize = file.getSize();
+            long maxFileSize = 5 * 1024 * 1024;
+
+            if (fileSize > maxFileSize) {
+                return "File size must be less the or equal 5MB";
+            }
+
+            if (
+                    !fileOriginalName.endsWith(".jpg") && !fileOriginalName.endsWith(".png") && !fileOriginalName.endsWith(".jpeg")
+            ) {
+                return "Only JPG, PNG or JPEG files are allowed";
+            }
+
+            String fileExtension = fileOriginalName.substring(fileOriginalName.lastIndexOf("."));
+
+            String newFileName = fileName + fileExtension;
+
+            File folder= new File("src/main/resources/picture");
+
+            if (!folder.exists()) {
+                folder.mkdirs();
+            }
+
+            Path path = Paths.get("src/main/resources/picture/" + newFileName);
+
+        }catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 }
